cad9380641f22b1cfb2a2c5427708cfd298793df
tetsuo <ogunduz@gmail.com>
2014-11-19 11:42:11 +0200

    test parser

--- test.js
+++ test.js
@@ -10,24 +10,35 @@ test('sayfa', function (t) {
     t.equal(typeof next, 'function');
     t.equal(window.location.hash, '#/beep');
     setTimeout(function () {
-      window.location.assign('#/boop');
+      window.location.assign('#/boop?f=1.2&b=onur%40example.com&ba=world&ba=dunya');
     }, 1);
   }
   function boop1 (ctx, next) {
-    t.deepEqual(ctx, {
-      cid: 'c2', hash: '#/boop', name: 'boop',
-      previous: {
-        cid: 'c1', hash: '#/beep', name: 'beep', previous: null
-      }
-    });
-    t.equal(window.location.hash, '#/boop');
+    t.equal(ctx.cid, 'c2');
+    t.equal(ctx.hash, '#/boop?f=1.2&b=onur%40example.com&ba=world&ba=dunya');
+    t.equal(ctx.name, 'boop');
     next();
   }
   function boop2 (ctx) {
+    t.deepEqual(ctx.query, {
+      foo: '1.2',
+      bar: 'onur@example.com',
+      baz: ['world', 'dunya']
+    });
     t.end();
   }
   sayfa('/beep', beep);
   sayfa('/boop', boop1, boop2);
+  sayfa('/')
   t.equal(window.location.hash, '');
-  sayfa.start({ default_hash: '#/beep' });
+  sayfa.start({ 
+    default_hash: '#/beep',
+    schema: {
+      boop: {
+        foo: 'double',
+        bar: 'email',
+        baz: 'alpha_array' 
+      }
+    }
+  });
 });
